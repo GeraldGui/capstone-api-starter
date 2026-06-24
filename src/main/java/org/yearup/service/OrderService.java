c0a709be4055adcbfaea49a2c493e6cd486e61ac
Gerald G <geraldguignard2@gmail.com>
2026-06-24 13:48:34 -0400

    - `OrderService` - Created `OrderService` with `checkoutService` method to convert cart to order, save line items, and clear cart.

--- src/main/java/org/yearup/service/OrderService.java
+++ src/main/java/org/yearup/service/OrderService.java
@@ -1,4 +1,41 @@
 package org.yearup.service;
 
+import org.springframework.stereotype.Service;
+import org.yearup.models.*;
+import org.yearup.repository.OrderLineItemRepository;
+import org.yearup.repository.OrderRepository;
+
+@Service
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderLineItemRepository orderLineItemRepository;
+    private final ShoppingCartService shoppingCartService;
+
+    public OrderService(OrderRepository orderRepository, OrderLineItemRepository orderLineItemRepository, ShoppingCartService shoppingCartService) {
+        this.orderRepository = orderRepository;
+        this.orderLineItemRepository = orderLineItemRepository;
+        this.shoppingCartService = shoppingCartService;
+    }
+
+    public Order checkoutService(int userId) {
+        ShoppingCart shoppingCart = shoppingCartService.getByUserId(userId);
+        Order order = new Order();
+        order.setUserId(userId);
+        Order orderSaved = orderRepository.save(order);
+
+        for (ShoppingCartItem shoppingCartItem : shoppingCart.getItems().values()) {
+            OrderLineItem orderLineItem = new OrderLineItem();
+
+            orderLineItem.setOrderId(orderSaved.getOrderId());
+            orderLineItem.setProductId(shoppingCartItem.getProductId());
+            orderLineItem.setSalesPrice(shoppingCartItem.getProduct().getPrice());
+            orderLineItem.setQuantity(shoppingCartItem.getQuantity());
+
+            orderLineItemRepository.save(orderLineItem);
+        }
+
+        shoppingCartService.deleteCartService(userId);
+        return orderSaved;
+    }
 }
